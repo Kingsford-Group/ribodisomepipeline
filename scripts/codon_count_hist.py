7e079a1e956653e39156b1cfef4daebc26a51838
Hao Wang <hw1@cs.cmu.edu>
2015-06-03 21:57:07 -0400

    normalized codon count distribution with expon fit added

--- scripts/codon_count_hist.py
+++ scripts/codon_count_hist.py
@@ -4,15 +4,17 @@ import numpy as np
 import scipy.stats
 from ribomap_result_parser import *
 from ribofit_utils import *
+from io_utils import *
 
 def main():
-    if len(sys.argv)!=4:
-        print "Usage: python codon_count_hist.py cds_range.txt ref.fasta ribomap.base"
+    if len(sys.argv)!=5:
+        print "Usage: python codon_count_hist.py cds_range.txt ref.fasta ribomap.base odir"
         exit(1)
 
     cds_txt = sys.argv[1]
     tfasta = sys.argv[2]
     pfn = sys.argv[3]
+    odir = sys.argv[4]
     cds_range = get_cds_range(cds_txt)
     tseq = get_tseq(tfasta, cds_range)
     pb=parse_estimated_profile(pfn)
@@ -24,6 +26,7 @@ def main():
     # stop_codons = ["TAA", "TGA", "TAG"]
     # tAI = exclude_keys(tAI, stop_codons)
 
+    ensure_dir(odir)
     print "sum counts from all three frames"
     pc = codonp_from_basep(pb, cds_range, sum_frames)
 
@@ -37,8 +40,19 @@ def main():
 
     print "fitting lognormal"
     params = fit_lognormal(jam_free_codon)
-    plot_codon_hist_fitting(jam_free_codon, params, lognormal_pdf)
-    #evaluate_fit_pipeline(jam_free_codon, jam_codon, tAI, 1e-7, 3, fn_prefix=fn_prefix+"_Tuller")
+    fn_prefix = odir+get_file_core(pfn)+"_lognormal"
+    plot_codon_hist_fitting(jam_free_codon, params, lognormal_pdf, fn_prefix=fn_prefix)
+
+    print "fitting exponential"
+    jam_codon = {}
+    jam_free_codon = {}
+    jam_free_codon, jam_codon = group_cnts_by_codon(jam_codon, jam_free_codon, pc, tseq,
+                                                    rc_threshold=-1, start=start, stop=stop,
+                                                    filter_peak=True, normalization="mRNA",
+                                                    validate_trans_func=validate_profile)
+    params = fit_expon(jam_free_codon)
+    fn_prefix = odir+get_file_core(pfn)+"_exp"
+    plot_codon_hist_fitting(jam_free_codon, params, expon_pdf, jam_codon, fn_prefix=fn_prefix)
 
 if __name__ == "__main__": main()
     
